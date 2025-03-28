8a4c45b1dcc1fd5ff18b7555c146741bea9eabd2
Mads <mossamadison@gmail.com>
2025-03-27 20:39:56 -0500

    Add files via upload

---
+++ Terminal_game.py
@@ -0,0 +1,24 @@
+#Eldrodge: the game
+#imports
+import random
+
+#variables
+start = y
+
+#code
+print("""Hello Adventurer!
+Welcome to the fantastical world of Eldridge!""")
+
+print('Are you ready for your adventure to begin?')
+
+while start == 2
+    start = int(input('1. Yes, or 2. No? '))
+
+    if start == 1
+        print('Then we begin in the small village of Goatstadt...')
+    else
+        print('Prepare yourself for your adventure, ally! Are you ready now? ')
+
+
+
+
